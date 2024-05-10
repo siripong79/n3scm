c704bf758e52151426d047290da0180d0af15388
Siriphong suksumran <oazaatru@gmail.com>
2024-05-11 02:02:14 +0700

    add pluins framworks and setting tailwind + vuetify

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <div class="">
+      <v-btn prepend-icon="$vuetify" stacked> Button </v-btn>
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <button class="bg-indigo-500 opacity-100 text-white">tailwind</button>
+    </div>
+  </v-app>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
